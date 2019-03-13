86d098e86f0d52d247488d40ae77a7b4560ba792
Kirsten Winther <winther@stanford.edu>
2019-03-13 14:23:37 -0700

    version 0.1.5

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.md', 'r') as f:
 
 setuptools.setup(
     name="Cathub",
-    version="0.1.4",
+    version="0.1.5",
     url="https://github.com/SUNCAT-Center/CatHub",
 
     author="Kirsten Winther",
