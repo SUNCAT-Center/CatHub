e59ac55b1865d4b014c2b96b143bfc5ecc7bf470
Viswanath Pasumarthi <pasumart@stanford.edu>
2020-10-16 00:24:11 -0700

    define function to map chemical symbols in formula to number of atoms

--- cathub/catmap_interface.py
+++ cathub/catmap_interface.py
@@ -1,6 +1,9 @@
+import re
+
 import pandas as pd
 from sqlalchemy import create_engine 
 from ase.db import connect
+from ase.data import chemical_symbols, atomic_numbers
 from tabulate import tabulate
 
 
@@ -12,7 +15,6 @@ class CatmapInterface():
     def write_input(self):
         # function to write input file
         a = 1
-        
         
 def db_to_dataframe(table_name, filename):
     "Read cathub .db file into pandas dataframe"
@@ -50,6 +52,10 @@ def write_energies(db_filepath, critical_density, reference_gases):
         if row.formula in reference_gases:
             relative_energy = 0.0
         else:
+            chemical_symbols_dict = formula_to_chemical_symbols(row.formula)
+            for chemical_symbol in chemical_symbols_dict.keys():
+                count = chemical_symbols_dict[chemical_symbol]
+                breakpoint()
             relative_energy = row.energy
 
         species.append(row.formula)
@@ -62,4 +68,54 @@ def write_energies(db_filepath, critical_density, reference_gases):
     with open(energies_filepath, 'w') as energies_file:
         df.to_string(energies_file, index=False)
     return None
+
+def formula_to_chemical_symbols(formula):
+    "Return dictionary mapping chemical symbols to number of atoms"
+
+    chemical_symbols_dict = {}
+
+    # split chemical formula string into alpha and numeric characters
+    regex = re.compile('(\d+|\s+)')
+    split_formula = regex.split(formula)
+    split_formula_list = []
+
+    # count number of formula units if any
+    start_index = 0
+    formula_unit_count = 1
+    if str.isdigit(split_formula[0]):
+        formula_unit_count = int(split_formula[0])
+        start_index = 1
+
+    # identify chemical symbol and map to its count
+    for string in split_formula[start_index:]:
+        if str.isdigit(string):
+            chemical_symbols_dict[last_chemical_symbol] = int(string)
+        else:
+            if len(string) == 0:
+                pass
+            elif len(string) == 1:
+                last_chemical_symbol = string
+                chemical_symbols_dict[last_chemical_symbol] = 1
+            elif len(string) == 2:
+                if string in chemical_symbols:
+                    last_chemical_symbol = string
+                    chemical_symbols_dict[last_chemical_symbol] = 1
+                else:
+                    chemical_symbols_dict[string[0]] = 1
+                    last_chemical_symbol = string[1]
+                    chemical_symbols_dict[last_chemical_symbol] = 1
+            elif len(string) == 3:
+                if string[0] in chemical_symbols:
+                    chemical_symbols_dict[string[0]] = 1
+                    last_chemical_symbol = string[1:]
+                    chemical_symbols_dict[last_chemical_symbol] = 1
+                else:
+                    chemical_symbols_dict[string[:2]] = 1
+                    last_chemical_symbol = string[2]
+                    chemical_symbols_dict[last_chemical_symbol] = 1
+
+    # multiply number of atoms for each chemical symbol with number of formula units
+    for key in chemical_symbols_dict.keys():
+        chemical_symbols_dict[key] = formula_unit_count * chemical_symbols_dict[key]
+    return chemical_symbols_dict
 
