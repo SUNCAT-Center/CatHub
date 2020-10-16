6f2c766d4a7df832b33d6e74daf5774dbe547473
Viswanath Pasumarthi <pasumart@stanford.edu>
2020-10-15 22:42:05 -0700

    setup function to write corrected energies to file. add definitions for gases.

--- cathub/catmap_interface.py
+++ cathub/catmap_interface.py
@@ -1,6 +1,8 @@
-from pandas import read_sql_table
+import pandas as pd
 from sqlalchemy import create_engine 
+from ase.db import connect
 
+from tabulate import tabulate
 
 class CatmapInterface():
     def __init__(self, table_name, filename):
@@ -22,6 +24,33 @@ def db_to_dataframe(table_name, filename):
     cnx = create_engine(sql_url).connect()
 
     # table will be returned as a dataframe
-    df = read_sql_table(table_name, cnx)
+    df = pd.read_sql_table(table_name, cnx)
     return df
+
+def write_energies(db_filepath, critical_density):
+    "Write formation energies to energies.txt after applying free energy corrections"
+
+    table_name = 'systems'
+    df = db_to_dataframe(table_name, str(db_filepath))
+    gas_ids = list(df.id[df.mass / df.volume < critical_density])
+
+    db = connect(str(db_filepath))
+    gas_select_rows = [list(db.select(id=gas_id))[0] for gas_id in gas_ids]
+    surface, site, species, formation_energies = [], [], [], []
+    for row in gas_select_rows:
+        mass = row.mass
+        volume = row.volume
+        if mass / volume < critical_density:
+            surface.append('None')
+            site.append('gas')
+        species.append(row.formula)
+        formation_energies.append(f'{row.energy:.3f}')
+
+    df = pd.DataFrame(list(zip(surface, site, species, formation_energies)),
+                      columns=['Surface Name', 'Site Name', 'Species Name', 'Formation Energy'])
+
+    energies_filepath = db_filepath.parent / 'energies.txt'
+    with open(energies_filepath, 'w') as energies_file:
+        df.to_string(energies_file, index=False)
+    return None
 
