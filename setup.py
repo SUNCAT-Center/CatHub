f47c88f737cdae7d610cbf2193d739072d7b4918
Kirsten Winther <winther@stanford.edu>
2021-04-05 11:49:10 -0700

    remove support for python3.5 since not supported by recent ASE

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('requirements.txt', 'r') as f:
     requirements = f.readlines()
     git_requirements = [f.strip('\n') for f in requirements if 'git' in f]
     requirements = [f for f in requirements if 'git' not in f]
-
+1
 with open('README.md', 'r') as f:
     readme = f.read()
 
@@ -26,14 +26,13 @@ setuptools.setup(
     package_dir={'cathub': 'cathub'},
     entry_points={'console_scripts': ['cathub=cathub.cli:cli']},
     install_requires=requirements,
-    python_requires='>=3.5',
+    python_requires='>=3.6',
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'Topic :: Scientific/Engineering :: Chemistry',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
