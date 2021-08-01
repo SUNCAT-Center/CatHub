3a5d577bb7a7c5b423159b33c8dec6eef0b15738
Viswanath Pasumarthi <pasumart@stanford.edu>
2021-08-01 01:31:13 -0700

    define plot_free_energy_diagram function

--- cathub/cat_energy.py
+++ cathub/cat_energy.py
@@ -4,6 +4,7 @@ from pathlib import Path
 
 import numpy as np
 import pandas as pd
+from matplotlib import pyplot as plt
 from sqlalchemy import create_engine 
 from ase.db import connect
 from ase.neb import fit0
@@ -1134,6 +1135,56 @@ def get_free_energy_change_rxn_mechanism(df, rxn_mechanism):
     for rxn_expression in rxn_mechanism:
         free_energy_change += get_free_energy_change_reaction(df, rxn_expression)
     return free_energy_change
+
+def plot_free_energy_diagram(df, rxn_mechanisms, labels):
+    title_size = 12
+    font_size = 10
+    label_size = 8
+    tick_size = 6
+    line_width = 2
+    marker_size = 2
+    color_list = ['#000000', '#a6611a', '#018571', '#ca0020', '#2c7bb6']
+    fig, ax = plt.subplots()
+    for rxn_mechanism_index, rxn_mechanism in enumerate(rxn_mechanisms):
+        color_value = color_list[rxn_mechanism_index]
+        num_steps = len(rxn_mechanism)
+        energy_data = np.zeros((num_steps, 3))
+        for step_index, rxn_expression in enumerate(rxn_mechanism):
+            (reactant_dict, product_dict,
+             ts_state, _) = read_reaction_expression_data(rxn_expression)
+            for reactant, num_reactants in reactant_dict.items():
+                energy_data[step_index, 0] += num_reactants * get_free_energy_change_species(df, reactant)
+            for product, num_products in product_dict.items():
+                energy_data[step_index, 1] += num_products * get_free_energy_change_species(df, product)
+            if ts_state:
+                ts_state += 'star'
+                energy_data[step_index, 2] += get_free_energy_change_species(df, ts_state)
+            if step_index == 0:
+                ax.plot([step_index + 0.75, step_index + 1.25],
+                        [energy_data[step_index, 0]] * 2, color=color_value,
+                        linewidth=line_width, label=labels[rxn_mechanism_index])
+            ax.plot([step_index + 1.75, step_index + 2.25],
+                    [energy_data[step_index, 1]] * 2, linewidth=line_width,
+                    color=color_value)
+            if energy_data[step_index, 2]:
+                p = np.polyfit([step_index + 1.25, step_index + 1.50, step_index + 1.75],
+                               [energy_data[step_index, 0], energy_data[step_index, 2], energy_data[step_index, 1]],
+                               2)
+                x_data = np.linspace(step_index + 1.25, step_index + 1.75, 100)
+                y_data = (np.poly1d(p))(x_data)
+                ax.plot(x_data, y_data, linewidth=line_width, color=color_value)
+            else:
+                ax.plot([step_index + 1.25, step_index + 1.75],
+                        [energy_data[step_index, 0], energy_data[step_index, 1]],
+                        linewidth=line_width, color=color_value)
+
+    figure_name = 'Free Energy Diagram.png'
+    plt.xlabel('Step', fontsize=font_size)
+    plt.ylabel('Energy (eV)', fontsize=font_size)
+    plt.legend()
+    plt.tight_layout()
+    plt.savefig(figure_name, dpi=600)
+    return None
 
 def populate_chemical_symbols_dict(chemical_symbols_dict, last_chemical_symbol):
     if last_chemical_symbol in chemical_symbols_dict:
