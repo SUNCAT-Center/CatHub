dc03ba5608e1a73b458ad14f62b171e974be048d
Viswanath Pasumarthi <pasumart@stanford.edu>
2022-05-19 23:47:35 -0700

    test function for formula_to_chemical_symbols

--- cathub/tests/test_catenergy.py
+++ cathub/tests/test_catenergy.py
@@ -8,6 +8,8 @@ import unittest
 
 import yaml
 
+from cathub.cat_energy.conversion import formula_to_chemical_symbols
+
 
 DESIRED_SURFACE = 'Cu'
 DESIRED_FACET = '100'
@@ -37,6 +39,23 @@ class CatEnergyTestCase(unittest.TestCase):
                 print(exc)
         config_params = ReturnValues(params)
         return config_params
+
+    def test2_chemical_formula_to_symbols(self):
+        '''
+        Test function to convert formula to dictionary of chemical symbols
+        '''
+        species_list = ['H2', 'CO', '2CCO', 'OCCOH']
+        result_check_list = [{'H': 2},
+                             {'C': 1, 'O': 1},
+                             {'C': 4, 'O': 2},
+                             {'O': 2, 'C': 2, 'H': 1}]
+        chemical_symbols_dict_list = []
+        for index, species_value in enumerate(species_list):
+            chemical_symbols_dict_list[index] = formula_to_chemical_symbols(
+                                                                species_value)
+
+        for index, result_dict in enumerate(result_check_list):
+            assert result_dict == chemical_symbols_dict_list[index]
 
 if __name__ == '__main__':
     unittest.main()
