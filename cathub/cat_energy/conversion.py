7a5f12b4fea50e67fc634ec6f52b1f28ed804697
pasumart <pasumart@stanford.edu>
2024-07-22 16:40:21 -0400

    Update docstrings for conversion functions with detailed descriptions and parameters

--- cathub/cat_energy/conversion.py
+++ cathub/cat_energy/conversion.py
@@ -16,7 +16,27 @@ PHI_REF = 4.44  # experimental work function of SHE relative to vacuum at  0 V
 
 def read_reaction_expression_data(rxn_expression):
     '''
-    function to read reaction expression data to return its components
+    Read reaction expression data and return its components.
+
+    This function parses a reaction expression string to extract its reactants, 
+    products, transition state, and beta value. It handles both reversible and 
+    irreversible reactions and processes the species names to conform to CatMAP 
+    style.
+
+    Parameters:
+    -----------
+    rxn_expression : str
+        The reaction expression string to be parsed. It may include a beta value 
+        separated by a semicolon.
+
+    Returns:
+    --------
+    tuple
+        A tuple containing:
+        - reactant_dict (dict): Dictionary of reactant species with their quantities.
+        - product_dict (dict): Dictionary of product species with their quantities.
+        - ts_state (str): Transition state species, if present.
+        - beta (float): Beta value for the reaction, default is 0.0 if not specified.
     '''
     discard_species_list = ['*_t', '_t', '_g']
     reactant_discard_species_list = ['_t', '_g']
@@ -89,9 +109,26 @@ def read_reaction_expression_data(rxn_expression):
         ts_state = ts_state.replace(discard_species, '')
     return (reactant_dict, product_dict, ts_state, beta)
 
+
 def get_catmap_style_species(species):
     '''
-    return species name in CatMAP style
+    Return species name in CatMAP style.
+
+    This function converts a species name to the CatMAP style format and determines 
+    the number of species present. It handles different formats of species names, 
+    including gas and adsorbed species.
+
+    Parameters:
+    -----------
+    species : str
+        The species name to be converted.
+
+    Returns:
+    --------
+    tuple
+        A tuple containing:
+        - catmap_species (str): The species name in CatMAP style.
+        - num_species (float): The number of species.
     '''
     if 'H_g' in species:
         split_species = re.split('(\\d+)', species)
@@ -122,9 +159,26 @@ def get_catmap_style_species(species):
             catmap_species = catmap_species.replace('_g', 'gas')
     return (catmap_species, num_species)
 
+
 def populate_chemical_symbols_dict(chemical_symbols_dict, last_chemical_symbol):
     '''
-    Function to populate dictionary of chemical symbols
+    Populate dictionary of chemical symbols.
+
+    This function updates a dictionary of chemical symbols by incrementing the count 
+    of a specified chemical symbol. If the chemical symbol is not already present 
+    in the dictionary, it is added with a count of one.
+
+    Parameters:
+    -----------
+    chemical_symbols_dict : dict
+        Dictionary containing chemical symbols as keys and their counts as values.
+    last_chemical_symbol : str
+        The chemical symbol to be added or incremented in the dictionary.
+
+    Returns:
+    --------
+    dict
+        The updated dictionary with the specified chemical symbol's count incremented.
     '''
     if last_chemical_symbol in chemical_symbols_dict:
         chemical_symbols_dict[last_chemical_symbol] += 1
@@ -132,11 +186,24 @@ def populate_chemical_symbols_dict(chemical_symbols_dict, last_chemical_symbol):
         chemical_symbols_dict[last_chemical_symbol] = 1
     return chemical_symbols_dict
 
+
 def formula_to_chemical_symbols(formula):
     '''
-    Return dictionary mapping chemical symbols to number of atoms
-    '''
+    Return dictionary mapping chemical symbols to number of atoms.
 
+    This function parses a chemical formula string and returns a dictionary 
+    mapping each chemical symbol to the number of atoms present in the formula.
+
+    Parameters:
+    -----------
+    formula : str
+        The chemical formula as a string.
+
+    Returns:
+    --------
+    dict
+        Dictionary containing chemical symbols as keys and their counts as values.
+    '''
     chemical_symbols_dict = {}
 
     # split chemical formula string into alpha and numeric characters
@@ -177,40 +244,157 @@ def formula_to_chemical_symbols(formula):
                                       * chemical_symbols_dict[key])
     return chemical_symbols_dict
 
+
 def u_she_to_u_rhe(u_she, temp, pH):
     '''
-    Convert SHE potential to RHE potential
+    Convert SHE potential to RHE potential.
+
+    This function converts the Standard Hydrogen Electrode (SHE) potential to 
+    the Reversible Hydrogen Electrode (RHE) potential based on temperature and pH.
+
+    Parameters:
+    -----------
+    u_she : float
+        Standard Hydrogen Electrode potential.
+    temp : float
+        Temperature in Kelvin.
+    pH : float
+        pH value of the system.
+
+    Returns:
+    --------
+    float
+        The converted Reversible Hydrogen Electrode potential.
     '''
     u_rhe = u_she + KB * temp * np.log(10) * pH
     return u_rhe
 
+
 def u_she_to_field(u_she, u_pzc, d):
     '''
-    Convert SHE potential to electric field
+    Convert SHE potential to electric field.
+
+    This function converts the Standard Hydrogen Electrode (SHE) potential to 
+    the electric field based on the potential of zero charge (PZC) and the 
+    distance over which the potential difference is applied.
+
+    Parameters:
+    -----------
+    u_she : float
+        Standard Hydrogen Electrode potential.
+    u_pzc : float
+        Potential of zero charge.
+    d : float
+        Distance between the positively (center of the Helmholtz plane) and 
+        negatively charged planes.
+
+    Returns:
+    --------
+    float
+        The computed electric field.
     '''
     field = (u_she - u_pzc) / d
     return field
 
+
 def u_rhe_to_field(u_rhe, pH, u_pzc, d, temp):
     '''
-    Convert RHE potential to electric field
+    Convert RHE potential to electric field.
+
+    This function converts the Reversible Hydrogen Electrode (RHE) potential 
+    to the Standard Hydrogen Electrode (SHE) potential and then calculates 
+    the electric field based on the potential of zero charge (PZC) and the 
+    distance between the positively (center of the Helmholtz plane) and 
+    negatively charged planes.
+
+    Parameters:
+    -----------
+    u_rhe : float
+        Reversible Hydrogen Electrode potential.
+    pH : float
+        pH value of the system.
+    u_pzc : float
+        Potential of zero charge.
+    d : float
+        Distance between the positively (center of the Helmholtz plane) and 
+        negatively charged planes.
+    temp : float
+        Temperature in Kelvin.
+
+    Returns:
+    --------
+    tuple
+        A tuple containing:
+        - u_she (float): The converted Standard Hydrogen Electrode potential.
+        - field (float): The computed electric field.
     '''
     u_she = u_rhe - KB * temp * np.log(10) * pH
     field = (u_she - u_pzc) / d
     return (u_she, field)
 
+
 def field_to_voltage(field, pH, u_pzc, d, temp):
     '''
-    Convert electric field to SHE/RHE potential
+    Convert electric field to SHE/RHE potential.
+
+    This function converts an electric field to the Standard Hydrogen Electrode (SHE) 
+    potential and the Reversible Hydrogen Electrode (RHE) potential based on the 
+    potential of zero charge (PZC), the distance between the positively (center of the 
+    Helmholtz plane) and negatively charged planes, the pH value, and the temperature.
+
+    Parameters:
+    -----------
+    field : float
+        The electric field.
+    pH : float
+        pH value of the system.
+    u_pzc : float
+        Potential of zero charge.
+    d : float
+        Distance between the positively (center of the Helmholtz plane) and 
+        negatively charged planes.
+    temp : float
+        Temperature in Kelvin.
+
+    Returns:
+    --------
+    tuple
+        A tuple containing:
+        - u_she (float): The Standard Hydrogen Electrode potential.
+        - u_rhe (float): The Reversible Hydrogen Electrode potential.
     '''
     u_she = field * d + u_pzc
     u_rhe = u_she + KB * temp * np.log(10) * pH
     return (u_she, u_rhe)
 
+
 def get_rhe_contribution(u_rhe, species_value, reference_gases,
                          reactants=None, beta=None):
     '''
-    Compute the RHE-scale contribution towards free energy
+    Compute the RHE-scale contribution towards free energy.
+
+    This function calculates the RHE-scale contribution to the free energy change 
+    based on the Reversible Hydrogen Electrode (RHE) potential, the species being 
+    analyzed, and the reference gases. It considers whether the species is a 
+    reactant or a product in the reaction mechanism.
+
+    Parameters:
+    -----------
+    u_rhe : float
+        Reversible Hydrogen Electrode potential.
+    species_value : str
+        The species for which the RHE contribution is being computed.
+    reference_gases : list of str
+        List of reference gas species.
+    reactants : dict, optional
+        Dictionary of reactant species with their quantities (default is None).
+    beta : float, optional
+        Beta value for the reaction (default is None).
+
+    Returns:
+    --------
+    float
+        The computed RHE-scale contribution to the free energy.
     '''
     # x CO + y (H++e-) = CxH(y-2x+2z)Oz + (x-z) H2O
     # x CO + y/2 H2 = CxH(y-2x+2z)Oz + (x-z) H2O
@@ -264,12 +448,42 @@ def get_rhe_contribution(u_rhe, species_value, reference_gases,
 
     return rhe_energy_contribution
 
+
 def get_electric_field_contribution(
         field_effects, species_value, reference_gases, reactants=None,
         beta=None):
     '''
-    Compute the contribution of electric field towards the free energies of
-    adsorption energies
+    Compute the contribution of electric field towards the free energies of adsorption energies.
+
+    This function calculates the contribution of the electric field to the free energies 
+    of adsorption for a given species. It considers both RHE-scale and SHE-scale dependencies.
+
+    Parameters:
+    -----------
+    field_effects : dict
+        Dictionary containing field effect parameters such as:
+            'she_voltage' (float): Standard Hydrogen Electrode voltage.
+            'pzc_voltage' (float): Potential of zero charge.
+            'd' (float): Distance between the positively (center of the Helmholtz plane) 
+                         and negatively charged planes.
+            'U_RHE' (float): Reversible Hydrogen Electrode potential.
+            'mu' (dict): Dipole moment values for species.
+            'alpha' (dict): Polarizability values for species.
+    species_value : str
+        The species for which the electric field contribution is being computed.
+    reference_gases : list of str
+        List of reference gas species.
+    reactants : dict, optional
+        Dictionary of reactant species with their quantities (default is None).
+    beta : float, optional
+        Beta value for the reaction (default is None).
+
+    Returns:
+    --------
+    tuple
+        A tuple containing:
+        - rhe_energy_contribution (float): The RHE-scale contribution to the free energy.
+        - she_energy_contribution (float): The SHE-scale contribution to the free energy.
     '''
     she_voltage = field_effects['she_voltage']
     pzc_voltage = field_effects['pzc_voltage']
